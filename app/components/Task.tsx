102ac0f460aa1293627634d5801f984c9d493391
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2023-08-21 10:53:35 +0400

    add task edit functionality

--- app/components/Task.tsx
+++ app/components/Task.tsx
@@ -1,21 +1,62 @@
 "use client";
 
+import { editTodo } from "@/api";
 import { ITask } from "@/types/tasks";
+import { useRouter } from "next/navigation";
+import { FormEventHandler, useState } from "react";
+import { FiEdit, FiTrash2 } from "react-icons/fi";
+import Modal from "./Modal";
 
 interface TaskProps {
-    task: ITask;
-  }
+  task: ITask;
+}
 
+const Task: React.FC<TaskProps> = ({ task }) => {
+  const router = useRouter();
+  const [openModalEdit, setOpenModalEdit] = useState<boolean>(false);
+  const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>(task.text);
 
-  const Task: React.FC<TaskProps> = ({ task }) => {
-     
-    
-    return (
-       <tr kay={task.id}>
-        <td>{task.text}</td>
-        <td></td>
-       </tr>
-    );
+  const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    await editTodo({
+      id: task.id,
+      text: taskToEdit,
+    });
+    setOpenModalEdit(false);
+    router.refresh();
   };
-  
-  export default Task;
+  return (
+    <tr kay={task.id}>
+      <td>{task.text}</td>
+      <td className="flex gap-5">
+      <FiEdit
+          onClick={() => setOpenModalEdit(true)}
+          cursor='pointer'
+          className='text-blue-500'
+          size={25}
+        />
+        <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+          <form onSubmit={handleSubmitEditTodo}>
+            <h3 className='font-bold text-lg'>Edit task</h3>
+            <div className='modal-action'>
+              <input
+                value={taskToEdit}
+                onChange={(e) => setTaskToEdit(e.target.value)}
+                type='text'
+                placeholder='Type here'
+                className='input input-bordered w-full'
+              />
+              <button type='submit' className='btn'>
+                Submit
+              </button>
+            </div>
+          </form>
+        </Modal>
+        <FiTrash2 cursor="pointer" className="text-red-500" size={24} />
+      </td>
+    </tr>
+  );
+};
+
+export default Task;
